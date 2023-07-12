50d46c1e6485abeb11f41dd711ee0b7966a95b2c
Simon <simon.obad@gmail.com>
2023-07-12 16:06:20 +0200

    final

--- app/public/wp-content/themes/oceanwp-child/functions.php
+++ app/public/wp-content/themes/oceanwp-child/functions.php
@@ -1,17 +1,26 @@
 <?php
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
-            $uri = get_template_directory_uri() . '/rtl.css';
-        return $uri;
-    }
-endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+function my_theme_enqueue_styles()
+{
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style(
+    'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array('parent-style'),
+    wp_get_theme()->get('Version')
+  );
+}
+
+function add_admin_link_to_menu($items, $args)
+{
+  if ($args->menu == 'main-menu' && is_user_logged_in()) {
+    $items .= '<li class="admin-menu"><a href="' . admin_url() . '">Admin</a></li>';
+  }
+  return $items;
+}
+add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
+
 
 // END ENQUEUE PARENT ACTION
